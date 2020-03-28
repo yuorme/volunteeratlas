0a4a9921c6ba38b842a63e54bced939d0aeb77a3
yuorme <yuorme@gmail.com>
2020-03-28 01:51:15 -0400

    set logo height in pixels

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ def build_folium_map():
     return m._repr_html_()
 
 app.layout = html.Div(children=[
-    html.Center(html.Img(src=app.get_asset_url('banner.png'), height='25%', width='25%')),
+    html.Center(html.Img(src=app.get_asset_url('banner.png'), height=50)),
     dcc.Tabs(id="tabs", value='tab-map', children=[
         dcc.Tab(label='Interactive Map', value='tab-map', className='custom-tab', selected_className='custom-tab--selected-map'),
         dcc.Tab(label='Volunteer Signup Form', value='tab-volunteer', className='custom-tab', selected_className='custom-tab--selected-volform'),

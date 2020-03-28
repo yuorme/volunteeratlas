66263d2403283c341fda825bbd0b26414d045504
yuorme <yuorme@gmail.com>
2020-03-28 01:55:34 -0400

    about me writeup teaks

--- app.py
+++ app.py
@@ -177,19 +177,19 @@ about_text = dcc.Markdown('''
     COVID-19 is a global problem requiring large-scale local responses. VolunteerAtlas is trying to create a global online repository of volunteers to help deal with this growing crisis. Self isolation for the most at-risk individuals in our community will require essentials like food and medicine be delivered to their doorsteps. If you're a young, healthy person with no dependents, and have been practicing social distancing, maybe you’d like to help.
 
     ##### Privacy
-    We take your privacy seriously. Only your Given Name, Email Address and About Me sections will be shared on the website. All additional personal information will only be accessible by admins and will be used solely to confirm identities and protect those we are seeking to help.
-    Our system is also designed to protect your physical location. We only ask for a postal code (not your home address) and use that to generate an approximate location. We then add an additional 500m of random noise to this approximate location to further protect your privacy.
+    We take your privacy seriously. Only your **Given Name, Email Address** and **About Me** sections will be shared on the website. All additional personal information will only be accessible by admins and will be used solely to confirm identities and protect those we are seeking to help.
+    Our system is also designed to protect your physical location. We only ask for a postal code (not your home address) to get your approximate location. We then add an additional 500m of random noise to further protect your privacy.
 
     ##### FAQs
-    *Why might posting my information on this website be more helpful than just posting on Facebook/Twitter?*
+    **Why might posting my information on this website be more helpful than just posting on Facebook/Twitter?**
 
     Vulnerable people needing help the most are likely those who do not live in the same city as their close relatives/friends. Close relatives who live outside of the locality are less likely to see or be aware of Facebook groups or Twitter posts from localized help groups. Also, social media is ephemeral, if you are offering to help over a course of weeks or months, putting your information into a central repository is a more effective way to do it.
 
-    *What is the process for connecting volunteers with recipients?*
+    **What is the process for connecting volunteers with recipients?**
 
     Your approximate location will populate an interactive map and certain details from your responses will be available on your 'public profile'. Recipients will navigate through the map to select the most suitable volunteer based on their profile. A small group of admin will be involved in facilitating your volunteer effort behind the scenes.
 
-    *Buying groceries for more than just yourself might look to others like panic buying. What can I do if I'm confronted/prevented from shopping based on such suspicions?*
+    **Buying groceries for more than just yourself might look to others like panic buying. What can I do if I'm confronted/prevented from shopping based on such suspicions?**
 
     We are thinking about ways to implement a verification and authentication program. For the time being, we recommend you speak with store staff/management about your volunteerism and show them your registration on this website.
 ''')
